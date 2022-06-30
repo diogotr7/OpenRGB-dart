4c841ed70d5415e439c0473a46fad81a0275cf94
Diogo Trindade <diogotr7@gmail.com>
2022-06-30 18:35:19 +0100

    added StreamBuffer

--- lib/openrgb.dart
+++ lib/openrgb.dart
@@ -2,38 +2,29 @@ library openrgb;
 
 import 'dart:io';
 import 'dart:typed_data';
+import 'dart:async';
+
+import 'package:quiver/async.dart';
 
 import 'header.dart';
 import 'command.dart';
 
 class OpenRGBClient {
   final Socket _socket;
+  final StreamBuffer<int> _streamBuffer;
 
-  OpenRGBClient(this._socket);
+  OpenRGBClient(this._socket, this._streamBuffer);
 
   static Future<OpenRGBClient> connect(String host, int port) async {
-    final socket = await Socket.connect(host, port);
-    socket.listen((data) async {
-      //check if we have at least the header.
-      if (data.length < NetPacketHeader.headerLength) {
-        throw Exception('Incomplete packet');
-      }
-      final headerBytes = data.sublist(0, NetPacketHeader.headerLength);
-      final header = NetPacketHeader.parse(headerBytes);
-      if (data.length < header.dataLength + NetPacketHeader.headerLength) {
-        throw Exception('Incomplete packet');
-      }
-      //we have the whole packet.
-      final payload = data.sublist(
-          NetPacketHeader.headerLength, header.dataLength + NetPacketHeader.headerLength);
-
-      onPacketReceived(header, payload);
-    });
-
-    return OpenRGBClient(socket);
+    var socket= await Socket.connect(host, port);
+    var streamBuffer = StreamBuffer<int>();
+    
+    socket.cast<List<int>>().pipe(streamBuffer);
+    
+    return OpenRGBClient(socket, streamBuffer);
   }
 
-  Future<void> send(int commandId, Uint8List data, {int deviceId = 0}) async {
+  Future send(int commandId, Uint8List data, {int deviceId = 0}) async {
     final header = NetPacketHeader(
       deviceIndex: deviceId,
       commandId: commandId,
@@ -45,15 +36,21 @@ class OpenRGBClient {
     await _socket.flush();
   }
   
-  static void onPacketReceived(NetPacketHeader header , Uint8List payload) 
+  Future<Uint8List> readPacket() async
   {
-    //store the payload somewhere, idk?
+    var headerBytes = Uint8List.fromList(await  _streamBuffer.read(16));
+
+    var header = NetPacketHeader.parse(headerBytes);
+
+    var packetBytes = Uint8List.fromList(await _streamBuffer.read(header.dataLength));
+
+    return Future.value(packetBytes);
   }
 
   Future<int> getControllerCount() async {
     await send(CommandId.requestControllerCount, Uint8List(0));
     //wait for the result of onPacketReceived
-    final payload = new Uint8List(4);
+    final payload = Uint8List(4);
     final ByteData payloadByteData = ByteData.sublistView(payload);
     return payloadByteData.getUint32(0, Endian.little);
   }
